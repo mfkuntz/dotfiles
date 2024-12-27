e2e02fb9b3cc4d1a888d3e5e05b35c04bf9bd7ca
Matthew Kuntz <matt@mfkuntz.com>
2024-12-27 18:00:54 -0500

    yess!

--- dockSetup.sh
+++ dockSetup.sh
@@ -2,4 +2,7 @@
 
 defaults write com.apple.dock autohide-delay -int 0
 defaults write com.apple.dock autohide-time-modifier -float 0.4
+defaults write com.apple.dock size-immutable -bool yes
+# defaults write com.apple.dock size-immutable -bool no
+
 killall Dock
